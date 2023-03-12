febf6e7e2c09e3aec9abea2485fb92b0407ccd54
minhlong149 <minhlong9514@gmail.com>
2023-03-12 16:38:38 +0700

    Create login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="grid h-screen place-items-center">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
+
+function Login() {
+  return (
+    <form className="inline-flex flex-col gap-4">
+      <div className=" flex justify-between items-center gap-2">
+        <label className="" for="username">
+          Username:
+        </label>
+        <input className="" type="text" name="username" required />
+      </div>
+
+      <div className="flex justify-between items-center gap-2">
+        <label className="" for="password">
+          Password:
+        </label>
+        <input className="" type="password" name="password" required />
+      </div>
+
+      <input
+        className="rounded w-full cursor-pointer bg-blue-500 text-white px-4 py-2"
+        type="submit"
+        value="Login"
+      />
+
+      <div className="flex justify-between items-center gap-2">
+        <label>
+          <input
+            className="cursor-pointer mr-1"
+            type="checkbox"
+            name="saveInfo"
+            value="yes"
+          />
+          Remember me
+        </label>
+
+        <a className="hover:underline" href="#">
+          Create new account
+        </a>
+      </div>
+    </form>
+  );
+}
